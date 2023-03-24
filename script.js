75e0001d1bcea7f3305427ac6f09c35c2cafede7
Solange Demey <ssdemey@gmail.com>
2023-03-24 08:46:53 -0300

    Clase_5

---
+++ script.js
@@ -0,0 +1,5 @@
+//const imgRef = document.querySelector("#verde"); //capturar elemento
+const imgRef = document.getElementById ("verde"); //es mas efetivo para capurar elementos
+imgRef.addEventListener ("click", alert ("Frase motivadora")); //llamar a eventos
+
+console.log(imgRef);
